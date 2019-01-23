f41d4da4b303646235e0ecac164e211543bdbc24
dustin_chiu <hashtagracoon@gmail.com>
2019-01-23 21:52:26 +0800

    add facebook id/secret

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   mongoDBURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY
 };
