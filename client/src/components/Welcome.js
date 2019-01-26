80b74a98ca77fe023a40ed9e84cd851491cceee4
dustin_chiu <hashtagracoon@gmail.com>
2019-01-26 22:22:26 +0800

    display result frontend

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Welcome extends Component {
+class Welcome extends Component {
+
+  renderContent() {
+    if(!this.props.auth) {
+      return (
+        <h3>Warning: Once you login with Facebook, your likes list would be shown here!</h3>
+      );
+    }
+    else if(this.props.auth.provider === 'google') {
+      return (
+        <h3>Hello { this.props.auth.name }, you have logged in with Google!</h3>
+      );
+    }
+    else if(this.props.auth.provider === 'facebook') {
+      return (
+        <div>
+          <h3>Hello { this.props.auth.name }!</h3>
+          {
+            this.props.auth.likes.map((el) => {
+              return (<p>{ el }</p>);
+            })
+          }
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
-        <h3>
-          Once you login, your like list would be shown here!
-        </h3>
+        { this.renderContent() }
       </div>
     );
   }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Welcome);
